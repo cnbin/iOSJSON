24e98afe52904badb639edc7b7673197e30331fa
binbin <845040571@qq.com>
2015-08-14 11:14:07 +0800

    2015-8-14

---
+++ Exer/SYAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SYAppDelegate.h
+//  Exer
+//
+//  Created by Sauchye on 14-9-5.
+//  Copyright (c) 2014年 Sauchye. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SYAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
